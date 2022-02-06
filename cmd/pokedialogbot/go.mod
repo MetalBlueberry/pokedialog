6a346fa00ea12152fc50175373c9b754681ef480
metalblueberry <metalblueberry@gmail.com>
2022-02-06 20:11:24 +0100

    init

---
+++ cmd/pokedialogbot/go.mod
@@ -0,0 +1,5 @@
+module github.com/metalblueberry/pokedialog/cmd/pokedialogbot
+
+go 1.17
+
+require gopkg.in/telebot.v3 v3.0.0-20220205194740-8adfab48e8b2 // indirect
---
+++ cmd/pokedialogbot/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	b.Start()
+}
